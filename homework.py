9086e47411f330fbb11b17e0f918f9504c400ea4
Дмитрий Шинкарев <dsh.dev33@gmail.com>
2020-03-05 13:09:27 +0300

    first project / first check

--- homework.py
+++ homework.py
@@ -1,175 +1,147 @@
 import datetime as dt
-
-class Calculator:
-    def __init__(self, limit):
-        self.limit = limit
-        records = []
-
-    def GetCurrentSum(currency, limit):
-
-        USD_RATE  = 60
-        EURO_RATE = 70
-
-
-
-class CaloriesCalculator(Calculator):
-    def __init__(self, limit):
-        super().__init__(limit)
-        #records = []
-
-    def get_calories_remained(self):
-        # Определять, сколько ещё калорий можно/нужно получить сегодня
-
-        colory = currentlimitcalory()    
-        answer_calory = ''
-
-        if limit > currentlimitcalory:
-            n_text = limit - currentlimitcalory
-            answer_calory = f'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {n_text} кКал'
-        else:
-            answer_calory = 'Хватит есть!'
-        
-        return answer_calory
-
-
-    def add_record(self):
-        #Сохранять новую запись о приёме пищи— метод
-        self.records.append(Record)
-
-    
-    def get_today_stats():
-        #Считать, сколько калорий уже съедено сегодня — метод
-        print()
-        pass
-
-    def get_week_stats():
-        #Считать, сколько калорий получено за последние 7 дней — метод
-        print()
-        pass
-
-class CashCalculator(Calculator):
-    def __init__(self, limit):
-        super().__init__(limit)
-        #records = []
-
-
-    def get_today_cash_remained(currency):
-        #Определять, сколько ещё денег можно потратить сегодня в рублях, долларах или евро — метод
-        
-        CurentSum = GetCurrentSum(currency)
-        answer_limit = ''
-
-        if CurentSum > limit: # Есил лимит не достигнут
-            answer_limit = f'На сегодня осталось N {currency}'
-        elif CurentSum > limit: #Если лимит достигнут
-            answer_limit = 'Денег нет, держись'
-        elif CurentSum > limit: #если лимит превышен
-            answer_limit = 'Денег нет, держись: твой долг - N руб/USD/Euro'
-
-        return answer_limit
-
-    def add_record(Record)
-        #Сохранять новую запись о приёме пищи— метод
-        self.records.append(Record)
-
-    def get_today_stats():
-        #Считать, сколько денег потрачено сегодня
-        pass
-
-    def get_week_stats():
-        #Считать, сколько денег потрачено за последние 7 дней — метод 
-        pass
-
-
-
-
+from datetime import timedelta, datetime
 
 class Record:
     def __init__(self, amount = 0, comment = '', date = dt.datetime.now().date()):
+        super(Record, self).__init__()
         
         if amount == 0:
             print('Укажаите число трат')
             pass
+        
+        if type(date) == str:
+            date_format = '%d.%m.%Y'
+            date = dt.datetime.strptime(date, date_format).date()
 
         self.amount = amount
         self.comment = comment
         self.date = date
     
 
+class Calculator:
+    def __init__(self, limit):
+        self.limit = limit
+        self.records = []
 
+    #Сохранять новую запись о приёме пищи— метод
+    def add_record(self, Record):
+        self.records.append(Record)
 
-'''
-# для CashCalculator 
-r1 = Record(amount=145, comment="Безудержный шопинг", date="08.03.2019")
-r2 = Record(amount=1568, comment="Наполнение потребительской корзины", date="09.03.2019")
-r3 = Record(amount=691, comment="Катание на такси", date="08.03.2019")
+    def today(self):
+        return dt.datetime.now().date()
 
-# для CaloriesCalculator
-r4 = Record(amount=1186, comment="Кусок тортика. И ещё один.", date="24.02.2019")
-r5 = Record(amount=84, comment="Йогурт.", date="23.02.2019")
-r6 = Record(amount=1140, comment="Баночка чипсов.", date="24.02.2019"
-'''
+    def get_today_sum(self, records):
 
+        today = self.today()
+        sum_of_recods = 0    
 
-'''
-# создадим калькулятор денег с дневным лимитом 1000
-cash_calculator = CashCalculator(1000)
+        for rw in range(len(records)):
+            if records[rw].date == today:
+                    sum_of_recods += records[rw].amount
         
-# дата в параметрах не указана, 
-# так что по умолчанию к записи должна автоматически добавиться сегодняшняя дата
-cash_calculator.add_record(Record(amount=145, comment="кофе")) 
-# и к этой записи тоже дата должна добавиться автоматически
-cash_calculator.add_record(Record(amount=300, comment="Серёге за обед"))
-# а тут пользователь указал дату, сохраняем её
-cash_calculator.add_record(Record(amount=3000, comment="бар в Танин др", date="08.11.2019"))
-                
-print(cash_calculator.get_today_cash_remained("rub"))
-# должно напечататься
-# На сегодня осталось 565 руб
-'''
+        return sum_of_recods
+
+    def get_last_week_sum(self, records):
+
+        today = self.today()
+
+        date_list = []
+        ittr = 0
+
+        while ittr <= 6: # "Сегодня" тоже считаем 
+            find_date = today - timedelta(ittr)
+            date_list.append(find_date)
+            ittr += 1
 
 
-'''
-class Bird:
-    def __init__(self, name, size):
-            self.name = name
-            self.size = size
-    def show(self):       
-            print(f'{self.name} носит одежду размера «{self.size}».')
+        sum_week_records = 0    
 
-class Parrot(Bird):
-    def __init__(self, name, size, sound):
-        super().__init__(name, size)
-        self.sound = sound
-    def show(self):
-        print(f'{self.name} носит одежду размера «{self.size}» и {self.sound}.')
+        for rw in range(len(records)):
+             if records[rw].date in date_list:
+                sum_week_records += records[rw].amount
 
-class Predator(Bird):
-    def __init__(self, name, size, claws_size):
-        super().__init__(name, size)
-        self.claws_size = claws_size
-    def show(self):
-        print(f'{self.name} носит одежду размера «{self.size}» и когти размера {self.claws_size}.')
+        return sum_week_records    
 
-class Egg(Predator):
-    def show(self):
-        print(f'Из яйца вылупится птичка {self.name} размера «{self.size}» с когтями размера {self.claws_size}.')
-'''
+    def get_current_cash_sum(self, currency, records, EURO_RATE, USD_RATE):
+    
+        rate_currency = {'eur': EURO_RATE,'usd': USD_RATE,'rub': 1}
+        sum_today = self.get_today_sum(records) 
+        limit_currency = self.limit
+        
+        sum_today = sum_today / rate_currency[currency]
+        limit_currency = limit_currency / rate_currency[currency]
+        
+        balance = limit_currency - sum_today
+        
+        return  round(balance,2)
 
-'''
-class User:
-    def __init__(self, name, phone):
-        self.name = name
-        self.phone = phone
-    def show(self):
-        print(f'{self.name} ({self.phone})')
+    def get_current_limit_calory(self,records):
 
-# объявляем класс Friend, дочерний по отношению к классу User
-class Friend(User):
-    def show(self):
-        print(f'Имя: {self.name} || Телефон: {self.phone}')
+        current_calory_today = self.get_today_sum(records)
 
-# создаём объекты User и Friend
-father = User("Дюма-отец", "+33 3 23 96 23 30")
-son = Friend("Дюма-сын", "+33 3 23 96 23 30")
-'''
+        balance = self.limit - current_calory_today
+
+        return round(balance, 2)
+
+    #Считать, сколько калорий уже съедено сегодня — метод
+    def get_today_stats(self):
+
+        stats_today = self.get_today_sum(self.records)
+        return stats_today
+
+    #Считать, сколько калорий получено за последние 7 дней — метод    
+    def get_week_stats(self):
+
+        week_stat = self.get_last_week_sum(self.records)
+        return week_stat
+
+
+
+class CaloriesCalculator(Calculator):
+    def __init__(self, limit):
+        super().__init__(limit)
+
+
+    # Определять, сколько ещё калорий можно/нужно получить сегодня
+    def get_calories_remained(self):
+
+        balance = super().get_current_limit_calory(self.records)    
+        answer_calory = ''
+
+        if balance > 0:
+            answer_calory = f'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {balance} кКал'
+        else:
+            answer_calory = 'Хватит есть!'
+        
+        return answer_calory
+    
+
+class CashCalculator(Calculator):
+    
+    EURO_RATE = 70.00
+    USD_RATE = 60.00
+
+    def __init__(self, limit):
+        super().__init__(limit)
+
+
+    def get_today_cash_remained(self, currency):
+        #Определять, сколько ещё денег можно потратить сегодня в рублях, долларах или евро — метод
+        
+        list_of_currency = ['rub','usd','eur']
+
+        if not currency in list_of_currency:
+            print(f'Укажите валюту правильно: {list_of_currency[0]},{list_of_currency[1]},{list_of_currency[2]}')
+
+        print_currency = {'eur':'Euro','usd':'USD','rub':'руб'}    
+
+        limit = super().get_current_cash_sum(currency, self.records, CashCalculator.EURO_RATE, CashCalculator.USD_RATE)
+
+        if limit > 0: # Есил лимит не достигнут
+            answer_limit = f'На сегодня осталось {limit} {print_currency[currency]}'
+        elif limit == 0: #Если лимит достигнут
+            answer_limit = 'Денег нет, держись'
+        elif limit < 0: #если лимит превышен
+            answer_limit = f'Денег нет, держись: твой долг - {limit * -1} {print_currency[currency]}'
+
+        return answer_limit
